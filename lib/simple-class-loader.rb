4a807e3ba43d2ff33361f80ab7c5b32710f599c4
Kazuhiro Yamada <kyamada@sonix.asia>
2013-03-01 01:26:33 +0900

    Initial commit

---
+++ lib/simple-class-loader.rb
@@ -0,0 +1,9 @@
+require "simple-class-loader/version"
+
+module Simple
+  module Class
+    module Loader
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/simple-class-loader/version.rb
@@ -0,0 +1,7 @@
+module Simple
+  module Class
+    module Loader
+      VERSION = "0.0.1"
+    end
+  end
+end
